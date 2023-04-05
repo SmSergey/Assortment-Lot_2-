3ac3dc9f438796643a55e9556298f0f2bb49ca0a
SmSergey <ssmirnov379@gmail.com>
2023-04-05 13:06:59 +0300

    minor optimisations

--- src/main/groovy/com/app/lot_2/Lot2Application.groovy
+++ src/main/groovy/com/app/lot_2/Lot2Application.groovy
@@ -1,18 +1,10 @@
 package com.app.lot_2
 
-import com.app.lot_2.domain.check.Check
-import com.app.lot_2.domain.check.Position
 import com.app.lot_2.domain.check.repository.CheckRepository
-import com.app.lot_2.domain.user.User
 import com.app.lot_2.domain.user.repository.UserRepository
-import jakarta.annotation.PostConstruct
-import jakarta.transaction.TransactionManager
-import org.springframework.beans.factory.annotation.Autowire
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.transaction.annotation.Transactional
-import org.springframework.transaction.support.TransactionTemplate
 
 @SpringBootApplication
 class Lot2Application {
@@ -26,4 +18,5 @@ class Lot2Application {
     static void main(String[] args) {
         SpringApplication.run(Lot2Application, args)
     }
+
 }
